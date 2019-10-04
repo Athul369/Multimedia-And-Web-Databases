0c92b984748e81efd3ce563b3098d9c42c0d41ef
manojtiwaskar <26196603+manojtiwaskar@users.noreply.github.com>
2019-10-04 09:59:13 +0530

    Add files via upload
    # Script for extracting feature for all the models and store it in db.

---
+++ code/feature_descriptor.py
@@ -0,0 +1,44 @@
+from LocalBinaryPatterns import LBP
+from ColorMoments import CM
+from SIFT import SIFT
+from HOGmain import HOG
+import os
+import glob
+import pymongo
+
+# import dbtask
+
+client = pymongo.MongoClient('localhost', 27017)
+imagedb = client["imagedb"]
+models = imagedb["image_models"]
+
+def calculate_fd(path):
+    for image in glob.glob(os.path.join(path, "*.jpg")):
+        dict = {}
+        dict["_id"] = image[-16:]
+
+        md = CM(image)
+        lst = md.getFeatureDescriptors()
+        dict["CM"] = lst
+
+        md = LBP(image)
+        lst = md.getFeatureDescriptors()
+        dict["LBP"] = lst
+
+        md = SIFT(image)
+        lst = md.getFeatureDescriptors()
+        dict["SIFT"] = lst
+
+        md = HOG(image)
+        lst = md.getFeatureDescriptors()
+        lst = lst.tolist()
+        dict["HOG"] = lst
+
+        # dict["HOG"] = lst.tolist()
+        # print(type(lst.tolist()))
+
+        rec = mydb.image_models.insert_one(dict)
+
+## Main
+path = input("Enter Path: ")
+calculate_fd(path)
