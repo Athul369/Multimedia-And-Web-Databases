5eb6c63e5493b70f9cf0df97dc95c7b65c5e806a
Prashant Chauhan <pchauha7@asu.edu>
2019-10-20 20:14:23 -0700

    Adding script file

--- code/Task8.py
+++ code/Task8.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.decomposition import NMF
 import Visualizer as vz
 
-class Task8:
+class Task8(object):
 
     def metadataRead(self,filepath, k):
         metadataFile = pd.read_csv(filepath, sep=',', header=None)
@@ -14,21 +14,6 @@ class Task8:
         #                         'imageName', 'irregularities']
         metadataFile.columns = ['id', 'age', 'gender', 'skinColor', 'accessories', 'nailPolish', 'aspectOfHand',
                                 'Orientation', 'imageName', 'irregularities']
-        # print(metadataFile)
-        # imageList = []
-        # a = 0
-        # for image in glob.glob(os.path.join(imagepath, "*.jpg")):
-        #     a = a + 1
-        #     imageList.append(image[-16:])
-
-        # metadataFile = metadataFile[metadataFile['imageName'].isin(imageList)]
-        # print(metadataFile)
-        # print(a)
-
-        # metadataFile[['aspect', 'orientation']] = metadataFile.aspectOfHand.str.split(expand=True)
-
-        # print(metadataFile)
-        # metadataFile = metadataFile[['imageName', 'aspect', 'orientation', 'gender', 'accessories']]
 
         metadataFile = metadataFile[['imageName', 'aspectOfHand', 'Orientation', 'gender', 'accessories']]
         # print(metadataFile)
@@ -37,17 +22,11 @@ class Task8:
 
     def convertToBinaryMatrix(self,metaDataFrame, k):
         a = 0
-        # metaDataFrame['orientation'] = metaDataFrame['orientation'].replace(['left', 'right'], [0, 1])
         metaDataFrame['Orientation'] = metaDataFrame['Orientation'].replace(['left', 'right'], [0, 1])
-        # metaDataFrame['aspect'] = metaDataFrame['aspect'].replace(['dorsal', 'palmar'], [0, 1])
         metaDataFrame['aspectOfHand'] = metaDataFrame['aspectOfHand'].replace(['dorsal', 'palmar'], [0, 1])
         metaDataFrame['gender'] = metaDataFrame['gender'].replace(['male', 'female'], [0, 1])
-        # print(metaDataFrame)
         imageList = metaDataFrame['imageName'].tolist()
-        # print(imageList)
-        # metaDataFrame = metaDataFrame[['aspect', 'orientation', 'gender', 'accessories']]
         metaDataFrame = metaDataFrame[['aspectOfHand', 'Orientation', 'gender', 'accessories']]
-        # print(metaDataFrame)
         metaDataFrame['accessories'] = metaDataFrame['accessories'].astype(int)
         featureList = metaDataFrame.columns
         self.performNMF(metaDataFrame, k, imageList, featureList)
@@ -57,12 +36,10 @@ class Task8:
         a = 0
         b = 0
         metaDataMatrix = metaDataFrame.to_numpy()
-        # print(metaDataMatrix)
         nmf_ = NMF(n_components=k, init='random', random_state=0)
         W = nmf_.fit_transform(metaDataMatrix)
         H = nmf_.components_
-        # print(W)
-        # print(H)
+
         W = self.rescaleToBasis(W)
         img_space = []
         print("Top {} latent semantics in image-space".format(k))
@@ -100,18 +77,8 @@ class Task8:
         rescaled_array = np.divide(arr, row_magnitude[:, None])
         return rescaled_array
 
-
-    # Use 11k metadata set
-    # "C:\Users\tyler\Documents\Xfer to ASU Google Drive\CSE 515\Project\11k Hands Data\Metadata\HandInfo.csv"
-    # metadatapath = input("Enter Metadata Path: ")
-    # "C:\Users\tyler\Documents\Xfer to ASU Google Drive\CSE 515\Project\11k Hands Data\Hands"
-    # imagedatapath = input("Enter Image Dataset Path: ")
-
-    # metadataRead(r"C:\Users\tyler\Documents\Xfer to ASU Google Drive\CSE 515\Project\11k Hands Data\Metadata\HandInfo.csv",
-    #              r"C:\Users\tyler\Documents\Xfer to ASU Google Drive\CSE 515\Project\11k Hands Data\Hands")
-
     def run_task_8(self,metadatacsvpath,k):
         self.metadataRead(metadatacsvpath, k)
 
-t8=Task8()
-t8.run_task_8(r"C:\Users\shadab\Documents\MWDB\project\ImageMetadata.csv",6)
+# t8=Task8()
+# t8.run_task_8(r"C:\Users\shadab\Documents\MWDB\project\ImageMetadata.csv",6)
